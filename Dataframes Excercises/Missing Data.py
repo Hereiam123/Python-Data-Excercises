160a7149576796d901ba2e3fc2658ea9893c3a22
Brian De Maio <bdemaio1@gmail.com>
2019-07-07 20:10:30 -0400

    Fill missing values or drop values in NaN

--- Dataframes Excercises/Missing Data.py
+++ Dataframes Excercises/Missing Data.py
@@ -7,3 +7,16 @@ d = {'A': [1, 2, np.nan], 'B': [5, np.nan, np.nan], 'C': [1, 2, 3]}
 df = pd.DataFrame(d)
 
 print(df)
+
+# Drop Missing dataframe based on row
+print(df.dropna())
+
+# Drop Missing dataframe based on column
+print(df.dropna(axis=1))
+
+# Don't drop Row if row contains a value of 2.0
+print(df.dropna(thresh=2))
+
+# Fills NaN values with assigned value
+print(df.fillna(value='FILL VALUE'))
+print(df['A'].fillna(value=df['A'].mean()))
