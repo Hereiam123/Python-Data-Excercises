1b7c89aa56a941309c978dc62932b688f4a89496
Brian De Maio <bdemaio1@gmail.com>
2019-07-07 20:01:11 -0400

    Handling Missing information introduction

---
+++ Dataframes Excercises/Missing Data.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+d = {'A': [1, 2, np.nan], 'B': [5, np.nan, np.nan], 'C': [1, 2, 3]}
+
+# Build Dataframe from dictionary
+df = pd.DataFrame(d)
+
+print(df)
