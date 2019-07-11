55721b8eb54a68e30c1d3446b5b2ecfd9644e624
Brian De Maio <bdemaio1@gmail.com>
2019-07-10 22:28:10 -0400

    Starting on Group By

---
+++ Dataframes Excercises/GroubBy.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+data = {'Company': ['GOOG', 'GOOG', "MSFT", "MSFT", 'FB', 'FB'],
+        'Person': ['Sam', 'Charlie', 'Amy', 'Vanessa', 'Carl', 'Sarah'],
+        'Sale': [200, 120, 340, 124, 243, 350]}
+
+df = pd.DataFrame(data)
