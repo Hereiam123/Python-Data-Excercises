eb9a298fe9606e684d62fbba7df8b28205be97a7
Brian De Maio <bdemaio1@gmail.com>
2019-07-11 20:50:22 -0400

    Creating pivot table from Dataframe

--- Dataframes Excercises/DataFrame_Operations.py
+++ Dataframes Excercises/DataFrame_Operations.py
@@ -31,3 +31,28 @@ print(df['col3'].apply(len))
 
 # Using lambda functions
 print(df['col2'].apply(lambda x: x*2))
+
+# Remove column based on column specification
+#print(df.drop('col1', axis=1, inplace=True))
+
+# Call out dataframe column names
+print(df.columns)
+
+# Call out index values
+print(df.index)
+
+# Sort values based on column value
+print(df.sort_values('col2'))
+
+# Check if any element is null
+print(df.isnull())
+
+# Working with pivot tables
+data = {'A': ['foo', 'foo', 'foo', 'bar', 'bar', 'bar'],
+        'B': ['one', 'one', 'two', 'two', 'one', 'one'],
+        'C': ['x', 'y', 'x', 'y', 'x', 'y'],
+        'D': [1, 3, 2, 5, 4, 1]}
+
+df = pd.DataFrame(data)
+
+print(df.pivot_table(values='D', index=['A', 'B'], columns=['C']))
