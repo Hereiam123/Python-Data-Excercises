bd9146a22acb72310fafeab9bb3114ebcd78a77b
Brian De Maio <bdemaio1@gmail.com>
2019-07-11 23:14:29 -0400

    Reading in SQL data

--- Dataframes Excercises/Data_Input_and_Output.py
+++ Dataframes Excercises/Data_Input_and_Output.py
@@ -1,3 +1,4 @@
+from sqlalchemy import create_engine
 import pandas as pd
 
 # Read in a CSV
@@ -13,3 +14,15 @@ print(pd.read_csv('CSV Content/My_output.csv'))
 print(pd.read_excel('CSV Content/Excel_Sample.xlsx', sheet_name="Sheet1"))
 df = pd.read_excel('CSV Content/Excel_Sample.xlsx', sheet_name="Sheet1")
 df.to_excel('CSV Content/Excel_Sample2.xlsx', sheet_name="NewSheet")
+
+# Read in html table data
+data = pd.read_html(
+    'https://www.fdic.gov/bank/individual/failed/banklist.html')
+print(data)
+
+# Write in SQL Data to temp SQL engine
+engine = create_engine('sqlite:///:memory:')
+df.to_sql('my_table', engine)
+sqldf = pd.read_sql('my_table', con=engine)
+
+print(sqldf)
