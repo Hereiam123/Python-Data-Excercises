c43f0492bc36ac179e320e522c8350fa5d73c148
Brian De Maio <bdemaio1@gmail.com>
2019-07-11 20:36:44 -0400

    Apply functions to dataframe elements

--- Dataframes Excercises/DataFrame_Operations.py
+++ Dataframes Excercises/DataFrame_Operations.py
@@ -1,2 +1,30 @@
 import numpy as np
 import pandas as pd
+
+df = pd.DataFrame({'col1': [1, 2, 3, 4],
+                   'col2': [444, 555, 666, 444],
+                   'col3': ['abc', 'def', 'ghi', 'xyz']})
+# Return rows
+print(df.head())
+
+# Return unique values
+print(df['col2'].unique())
+
+# Count of Unique values
+print(df['col2'].nunique())
+
+# Count of unique values, by values
+print(df['col2'].value_counts())
+
+# Select data in Dataframe conditionally
+print(df[(df['col1'] > 2) & (df['col2'] == 444)])
+
+
+def times2(x):
+    return x*2
+
+
+# Apply function to each element in each column
+print(df['col1'].apply(times2))
+
+print(df['col3'].apply(len))
