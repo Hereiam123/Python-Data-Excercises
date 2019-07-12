7274e3156a5b6bb7ab49d6a99b1fdafc9c5319c1
Brian De Maio <bdemaio1@gmail.com>
2019-07-11 23:07:47 -0400

    Reading and Writing to Excel File

--- Dataframes Excercises/Data_Input_and_Output.py
+++ Dataframes Excercises/Data_Input_and_Output.py
@@ -2,3 +2,14 @@ import pandas as pd
 
 # Read in a CSV
 print(pd.read_csv('CSV Content/example.csv'))
+
+# Write to a CSV
+df = pd.read_csv('CSV Content/example.csv')
+df.to_csv('CSV Content/My_output.csv', index=False)
+
+print(pd.read_csv('CSV Content/My_output.csv'))
+
+# Read in Excel File by Sheet
+print(pd.read_excel('CSV Content/Excel_Sample.xlsx', sheet_name="Sheet1"))
+df = pd.read_excel('CSV Content/Excel_Sample.xlsx', sheet_name="Sheet1")
+df.to_excel('CSV Content/Excel_Sample2.xlsx', sheet_name="NewSheet")
