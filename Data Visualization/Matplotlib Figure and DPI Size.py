ba64efefc5580715bc0dc3133149e88bfb93100b
Brian De Maio <bdemaio1@gmail.com>
2019-07-14 10:54:39 -0400

    Using plot object method

--- Data Visualization/Matplotlib Figure and DPI Size.py
+++ Data Visualization/Matplotlib Figure and DPI Size.py
@@ -5,4 +5,18 @@ import numpy as np
 x = np.linspace(0, 5, 11)
 y = x**2
 
-plt.plot(x, y)
+# Set figure size and DPI
+fig = plt.figure(figsize=(3, 2), dpi=100)
+ax = fig.add_axes([0, 0, 1, 1])
+
+ax.plot(x, y)
+
+# Figsize Object Method
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8, 2))
+axes[0].plot(x, y)
+axes[1].plot(y, x)
+
+# Tighten Layout
+plt.tight_layout()
+
+plt.show()
