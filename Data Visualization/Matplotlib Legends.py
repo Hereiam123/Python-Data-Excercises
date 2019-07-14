89d9aa35dd4523f2c0c2bba623b0b60b7d1b68bd
Brian De Maio <bdemaio1@gmail.com>
2019-07-14 11:02:51 -0400

    Creating legend for plot

--- Data Visualization/Matplotlib Legends.py
+++ Data Visualization/Matplotlib Legends.py
@@ -6,3 +6,14 @@ x = np.linspace(0, 5, 11)
 y = x**2
 
 print(y)
+
+fig = plt.figure()
+
+ax = fig.add_axes([0, 0, 1, 1])
+
+# Add legend to plot with labels
+ax.plot(x, x**2, label='X Squared')
+ax.plot(x, x**3, label='X Cubed')
+ax.legend()
+
+plt.show()
