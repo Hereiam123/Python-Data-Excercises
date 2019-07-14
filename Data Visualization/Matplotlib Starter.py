9a2b94b2062b681cc3b324a042d0bbe0ca9dc41c
Brian De Maio <bdemaio1@gmail.com>
2019-07-14 01:01:37 -0400

    Creating multiple axes for one plot

--- Data Visualization/Matplotlib Starter.py
+++ Data Visualization/Matplotlib Starter.py
@@ -25,6 +25,7 @@ plt.plot(y, x, 'b')
 fig = plt.figure()
 
 # Setting axes values
+#Posistion and Sizing
 axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
 # Plotting data on axes
@@ -32,6 +33,18 @@ axes.plot(x, y)
 axes.set_xlabel('X Label')
 axes.set_ylabel('Y Label')
 axes.set_title('Set Title')
+
+# Set two figures on one canvas
+fig = plt.figure()
+axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+axes2 = fig.add_axes([0.2, 0.5, 0.4, 0.3])
+
+axes1.plot(x, y)
+axes1.set_title('Bigger Plot')
+
+axes2.plot(y, x)
+
+axes2.set_title('Smaller Plot')
 
 # Show plots
 plt.show()
