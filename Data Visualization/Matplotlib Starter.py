995a3a50e4dad2095677c4ae7dbad8e8515ba7e7
Brian De Maio <bdemaio1@gmail.com>
2019-07-14 00:47:31 -0400

    Multiple plots

--- Data Visualization/Matplotlib Starter.py
+++ Data Visualization/Matplotlib Starter.py
@@ -10,5 +10,16 @@ print(y)
 # Plot out data points
 plt.plot(x, y)
 
+# Add X, Y label and Title of plot
+plt.xlabel('X label')
+plt.ylabel('Y label')
+plt.title('Title')
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y, 'r')
+plt.subplot(1, 2, 2)
+
+plt.plot(y, x, 'b')
+
 # Show plot
 plt.show()
