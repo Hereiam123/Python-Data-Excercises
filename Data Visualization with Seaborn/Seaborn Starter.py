3b1193bc7c2597196f897b4026e2ba23560ceea9
Brian De Maio <bdemaio1@gmail.com>
2019-07-20 12:37:26 -0400

    Dispaying Seaborn dataset via matplotlib

--- Data Visualization with Seaborn/Seaborn Starter.py
+++ Data Visualization with Seaborn/Seaborn Starter.py
@@ -1,6 +1,11 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Using built in dataset
 tips = sns.load_dataset('tips')
 
 print(tips.head())
+
+sns.distplot(tips['total_bill'])
+
+plt.show()
