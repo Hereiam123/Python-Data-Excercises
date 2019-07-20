46e146b4ea45ee32655127dbaca4c41ffba15d5e
Brian De Maio <bdemaio1@gmail.com>
2019-07-20 13:00:06 -0400

    Checking out pariplot and rugplot data distribution sets

--- Data Visualization with Seaborn/Seaborn Starter.py
+++ Data Visualization with Seaborn/Seaborn Starter.py
@@ -6,6 +6,30 @@ tips = sns.load_dataset('tips')
 
 print(tips.head())
 
-sns.distplot(tips['total_bill'])
+# Removing kernel density information(kde)
+sns.distplot(tips['total_bill'], kde=False, bins=30)
+
+# Combine two distplots by variant
+# Comparing total_bill value and tips
+# Hex kind value changes points from sctatterpot dots
+sns.jointplot(x='total_bill', y='tip', data=tips, kind='hex')
+
+# Regression line added
+sns.jointplot(x='total_bill', y='tip', data=tips, kind='reg')
+
+# Kde will show density within joint values
+sns.jointplot(x='total_bill', y='tip', data=tips, kind='kde')
+
+# Pair plot will act as a jointplot across all numerical columns
+# In a dataframe
+
+# Hue will allow string values in a column to be plotted
+# Based on listed column
+sns.pairplot(tips, hue='sex', palette='coolwarm')
+
+# Rugplot draws dashed plot of datapoint listed in column
+# Below
+sns.rugplot(tips['total_bill'])
+
 
 plt.show()
