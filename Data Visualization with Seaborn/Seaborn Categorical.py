22b16db6e8d5a945aac56a8396a04ebc4f3238bf
Brian De Maio <bdemaio1@gmail.com>
2019-07-21 12:06:04 -0400

    Violin plots are very interesting visually

--- Data Visualization with Seaborn/Seaborn Categorical.py
+++ Data Visualization with Seaborn/Seaborn Categorical.py
@@ -6,17 +6,16 @@ tips = sns.load_dataset('tips')
 print(tips.head())
 
 # Aggregate Categorical Data via a specific method, average by default
-sns.barplot(x='sex', y='total_bill', data=tips, estimator=np.std)
+#sns.barplot(x='sex', y='total_bill', data=tips, estimator=np.std)
 
 # Count plot, estimator is specifically number of occurences
-sns.countplot(x='sex', data=tips)
+#sns.countplot(x='sex', data=tips)
 
 # Box plot, shows distribution of categorical data
-sns.boxplot(x='day', y='total_bill', data=tips, hue='smoker')
+#sns.boxplot(x='day', y='total_bill', data=tips, hue='smoker')
 
 # Violin Plots, similar to boxplot
 # Show distribution of data across a category
-sns.violinplot(x='day', y='total_bill', data=tips)
-
+sns.violinplot(x='day', y='total_bill', data=tips, hue='sex', split=True)
 
 plt.show()
