6caea1aae326c0396a382a4991f3164fff33727c
Brian De Maio <bdemaio1@gmail.com>
2019-07-21 12:48:25 -0400

    A little linear reg styling

--- Data Visualization with Seaborn/Seaborn Regression.py
+++ Data Visualization with Seaborn/Seaborn Regression.py
@@ -10,3 +10,16 @@ sns.lmplot(x='total_bill', y='tip', data=tips, hue='sex',
            markers=['o', 'v'], scatter_kws={'s': 100})
 
 plt.show()
+
+# Vary plot by column value, and row value
+# Similar to grids
+sns.lmplot(x='total_bill', y='tip', data=tips, col='sex', row='time')
+
+plt.show()
+
+# Add a little more variation based on hue
+# Change aspect ratio of gridded plots
+sns.lmplot(x='total_bill', y='tip', data=tips,
+           col='day', hue='sex', aspect=0.6, height=8)
+
+plt.show()
