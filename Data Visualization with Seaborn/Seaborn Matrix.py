b1453f024c916dc256ec0453f7ce9c44614f0331
Brian De Maio <bdemaio1@gmail.com>
2019-07-21 12:24:38 -0400

    Cluster maps and heatmaps

---
+++ Data Visualization with Seaborn/Seaborn Matrix.py
@@ -0,0 +1,32 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+tips = sns.load_dataset('tips')
+flights = sns.load_dataset('flights')
+
+print(tips.head())
+
+print(flights.head())
+
+# Need to make Dataset into a Matrix, so the index value
+# represents a respective data point colum
+print(tips.corr())
+tc = tips.corr()
+
+# Heatmapping data in Matrix plot
+sns.heatmap(tc, annot=True, cmap='coolwarm')
+# plt.show()
+
+# Create Matrix from Flights dataframe
+fp = flights.pivot_table(index='month', columns='year', values='passengers')
+print(fp)
+
+# Use line color and line width to add a little seperation in data blocks
+sns.heatmap(fp, cmap='magma', linecolor='white', linewidths=1)
+# plt.show()
+
+# Cluster map, cluster columns based on similarity of data
+# points
+sns.clustermap(fp)
+plt.show()
