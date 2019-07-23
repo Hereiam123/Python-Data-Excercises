7eb3179c1f84c28d8cbea3c7133eed1c93cdfb05
hereiam123 <bdemaio1@gmail.com>
2019-07-22 22:13:02 -0400

    Outputting that awesome layout data!

--- Interactive Visualization with Plotly/Plotly Choropleth Maps.py
+++ Interactive Visualization with Plotly/Plotly Choropleth Maps.py
@@ -6,3 +6,14 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 print(__version__)
 
 init_notebook_mode(connected=True)
+
+data = dict(type='choropleth', locations=[
+            'AZ', 'CA', 'NY'], locationmode='USA-states', colorscale='Portland', text=['text 1', 'text 2', 'text 3'], z=[1.0, 2.0, 3.0], colorbar={'title': 'Colorbar Title'})
+
+print(data)
+
+layout = dict(geo={'scope': 'usa'})
+
+#Use go figure to output map
+choromap = go.Figure(data=[data], layout=layout)
+plot(choromap)
