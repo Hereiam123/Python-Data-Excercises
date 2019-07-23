9a287f62ce2822b1c87da4335bd464eff41f0125
hereiam123 <bdemaio1@gmail.com>
2019-07-22 22:07:24 -0400

    Checking out Graphical plotting

---
+++ Interactive Visualization with Plotly/Plotly Choropleth Maps.py
@@ -0,0 +1,8 @@
+import plotly.graph_objs as go
+import numpy as np
+import pandas as pd
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+print(__version__)
+
+init_notebook_mode(connected=True)
