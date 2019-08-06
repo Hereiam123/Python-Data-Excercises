c231f2be5ea50841d16334ec5d966224845d44fa
hereiam123 <bdemaio1@gmail.com>
2019-08-05 21:17:19 -0400

    Visualizing GDP data onto International map

--- Interactive Visualization with Plotly/Plotly Choropleth Maps Inter.py
+++ Interactive Visualization with Plotly/Plotly Choropleth Maps Inter.py
@@ -7,29 +7,15 @@ print(__version__)
 
 init_notebook_mode(connected=True)
 
-# Z is data represented
-data = dict(type='choropleth', locations=[
-            'AZ', 'CA', 'NY'], locationmode='USA-states', colorscale='Portland', text=['Arizona', 'California', 'New York'], z=[1.0, 2.0, 3.0], colorbar={'title': 'Colorbar Title'})
-
-print(data)
-
-# Define as a US states layout
-layout = dict(geo={'scope': 'usa'})
-
-# Use go figure to output map
-choromap = go.Figure(data=[data], layout=layout)
-plot(choromap)
-
-df = pd.read_csv('CSV Content/2011_US_AGRI_Exports')
+df = pd.read_csv('CSV Content/2014_World_GDP')
 print(df.head())
 
-# Markers
-data = dict(type='choropleth', colorscale='YlOrRd',
-            locations=df['code'], locationmode='USA-states', z=df['total exports'], text=df['text'], marker=dict(line=dict(color='rgb(255,255,255)', width=2)), colorbar={'title': 'Millions USD'})
+data = dict(type='choropleth', locations=df['CODE'], z=df['GDP (BILLIONS)'],
+            text=df['COUNTRY'], colorbar={'title': 'GDP in Billions USD'})
 
-layout = dict(title='2011 US Agriculture Exports by State', geo=dict(
-    scope='usa', showlakes=True, lakecolor='rgb(85,173,240)'))
+layout = dict(title='2014 Global GDP', geo=dict(
+    showframe=False, projection={'type': 'mercator'}))
 
-choromap2 = go.Figure(data=[data], layout=layout)
+choromap = go.Figure(data=[data], layout=layout)
 
-plot(choromap2)
+plot(choromap)
